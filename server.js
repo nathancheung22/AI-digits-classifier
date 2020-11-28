cd0d7214a754a2f0a67ae127971f0628c068a406
Nathan Cheung <nathancheung22@gmail.com>
2020-11-27 20:21:21 -0500

    moved tf model to localStorage

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 "use strict";
+const express = require("express");
+const path = require("path");
 
-const app = require("./express/server");
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Local app listening on port ${PORT}!`));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
+
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,21 @@
 import * as tf from "@tensorflow/tfjs";
+import * as myModel from "./model";
+
+const loadModelIfNotPresent = () => {
+  for (const [key, value] of Object.entries(myModel)) {
+    if (localStorage.getItem(key)) {
+      continue;
+    }
+
+    if (key === "weight_data") {
+      localStorage.setItem(`tensorflowjs_models/tfModel/${key}`, btoa(value));
+    } else {
+      localStorage.setItem(`tensorflowjs_models/tfModel/${key}`, JSON.stringify(value));
+    }
+  }
+};
 
 // exports an empty object, then asynchronously updates the object
-
 export const tfModel = { model: null };
-tf.loadLayersModel("https://zealous-agnesi-ec5e3a.netlify.app/model").then((model) => {
-  tfModel.model = model;
-  // runs the model on a bogus tensor to cache the model
-  const bogusMatrix = new Array(28).fill(new Array(28).fill([0]));
-  const bogusTensor = tf.tensor([bogusMatrix]);
-  model.predict(bogusTensor);
-});
+loadModelIfNotPresent();
+tf.loadLayersModel("localstorage://tfModel").then((model) => (tfModel.model = model));
